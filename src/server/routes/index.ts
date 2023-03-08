eddb67b35196d3a0a26881303429853d2ca5f615
Josimesio <jorossini@gmail.com>
2023-03-08 16:34:30 -0300

    feat: criando tsconfig e compilando arquivos js

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -3,31 +3,10 @@ import { StatusCodes} from 'http-status-codes';
 
 const router = Router();
 
-router.get('/', (_, res)=>{
-
-  return res.send('Teste get');
-});
-
-router.get('/teste', (_, res)=>{
-
-  return res.send('Teste get 2');
-});
-
-router.post('/teste', (req, res)=>{
-  console.log(req.body);
-  return res.json(req.body);
-});
-router.get('/', (_, res)=>{
-
-  return res.send('teste post');
-});
-
 router.post('/teste', (req, res)=>{
   console.log(req);
   return res.status(StatusCodes.UNAUTHORIZED).json(req.body);
 });
-
-
 
 
 export {router};
