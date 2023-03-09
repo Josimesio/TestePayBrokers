b72bbcfc442424471ec13ce86f1bf128c2b1bb87
Josimesio <jorossini@gmail.com>
2023-03-09 10:33:29 -0300

    feat: Criando validações dos campos de Produto_API_A

--- src/server/controllers/produto_api_A/Create.ts
+++ src/server/controllers/produto_api_A/Create.ts
@@ -1,20 +1,37 @@
 import { Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import * as yup  from 'yup';
 
 
 interface IProduto {
-  id: string;
-  Nome : string;
+  id: number;
+  Nome: string;
   Descricao: string;
-  Valor:string; 
-
+  Valor:number; 
 
 }
+const bodyValidation : yup.ObjectSchema<IProduto> = yup.object().shape({
+  id: yup.number().required().positive().integer(),
+  Nome: yup.string().required(),
+  Descricao: yup.string().required(),
+  Valor:yup.number().required()    
+});
 
-export const create = (req: Request<{},{},IProduto>,res: Response) => {
-  console.log(req.body); 
+export const create = async (req: Request<{},{},IProduto>,res: Response) => {
+  let validateData: IProduto | undefined = undefined;
+  try{
+    validateData = await bodyValidation.validate(req.body);
+  }catch (error){
+    const yupError = error as yup.ValidationError;
+    return res.json({
+      errors:{
+        default: yupError.message,
+      }
+    });
+  }
+
+  console.log(req.body.id); 
   
-
-
   return res.send('Create!');
 };
 
