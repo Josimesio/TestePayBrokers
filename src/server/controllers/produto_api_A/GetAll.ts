b726d6bb8fd8ffe1550b7327aa17d47e62962e2a
Josimesio <jorossini@gmail.com>
2023-03-11 18:15:17 -0300

    feat continuando configuracao do knex 2

--- src/server/controllers/produto_api_A/GetAll.ts
+++ src/server/controllers/produto_api_A/GetAll.ts
@@ -10,20 +10,22 @@ interface IQueryProps {
   limit?: number;
   filter?: string;
 }
-
-
 export const getAllValidation = validation((getSchema) => ({
-  /*query: getSchema<IQueryProps>(yup.object().shape({}))*/
-}));
-  /*(yup.object().shape({
-    page: yup.number().required().moreThan(0),
-    limit: yup.number().notRequired().moreThan(0),
-    filter: yup.string().notRequired(),
+  query: getSchema<IQueryProps>(yup.object().shape({
+    page: yup.number().optional().moreThan(0),
+    limit: yup.number().optional().moreThan(0),
+    filter: yup.string().optional(),
   })),
 }));
-*/
-export const getAll = async (req: Request<{}, {}, {}, IQueryProps>, res: Response) => {
-  console.log(req.query);
 
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send('Não implementado!');
+export const getAll = async (req: Request<{}, {}, {}, IQueryProps>, res: Response) => {
+  res.setHeader('access-control-expose-headers', 'x-total-count');
+  res.setHeader('x-total-count', 1);
+
+  return res.status(StatusCodes.OK).json([
+    {
+      id: 1,
+      nome: 'Caxias do Sul',
+    }
+  ]);
 };
